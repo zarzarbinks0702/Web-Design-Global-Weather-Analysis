3e5d58675933dc896f21b9710b986e58746a358f
Kaylie Sheehan <kaylie.sheehan@yahoo.com>
2021-05-17 21:07:21 -0500

    updates data
    converted timestamp to datetime

--- Resources/city_convert.py
+++ Resources/city_convert.py
@@ -1,6 +1,7 @@
 #importing dependencies
 import pandas as pd
 import csv
+from datetime import datetime as dt
 
 #read the csv file
 cities_csv_path = ('cities.csv')
@@ -11,5 +12,11 @@ with open(cities_csv_path, encoding="utf8") as cities_csv:
     #create a dataframe from the csvfile
     cities_df = pd.DataFrame(cities_csvreader, columns=cities_csv_header)
 
+    #convert date to datetime
+    for index, row in cities_df.iterrows():
+        date = int(row["Date"])
+        format_date = dt.fromtimestamp(date)
+        row["Date"] = format_date
+        
 #convert dataframe to html code
 cities_df.to_html(r'cities_table_html.html', index=False)
